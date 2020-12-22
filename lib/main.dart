7c48923c0afda16dcbb895d21b2f13fe170ad470
Arman Zahmatkesh <javad.zahmatkesh@gmail.com>
2020-12-23 02:14:08 +0300

    readme updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'حسابداری رایگان صفا',
+      title: 'Forex trading signal',
       debugShowCheckedModeBanner: false,
       theme: context.watch<MyProvider>().themeData,
       home: Dashboard()
